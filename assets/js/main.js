d4245533a1df9dcb6ac5719b454b479af058c1e3
Kydoimos <antifitamin@gmail.com>
2022-01-03 15:11:02 +0100

    feat: implement menu functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,25 @@
-js file init
+/*==================== SHOW MENU ====================*/
+
+const showMenu = (toggleID, navID) => {
+    const toggle = document.getElementById(toggleID);
+    const nav = document.getElementById(navID);
+
+    if (toggle && nav) {
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show-menu');
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu');
+
+/*==================== REMOVE MENU ====================*/
+
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu');
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
